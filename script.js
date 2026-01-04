52307fc36cd8cbfebb49bc67b48034c2b575c08e
sltnrfli <157139898+sltnrfli@users.noreply.github.com>
2026-01-04 18:57:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const noBtn = document.getElementById("noBtn");
 const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
 const loveText = document.getElementById("loveText");
+const music = document.getElementById("bgMusic");
 
 const reasons = [
   "Aku juga kangen kamu 🤍",
@@ -13,21 +14,34 @@ const reasons = [
   "Aku bersyukur punya kamu 💕",
 ];
 
-noBtn.addEventListener("touchstart", moveButton);
-noBtn.addEventListener("mouseover", moveButton);
-
+// tombol ENGGAK kabur
 function moveButton() {
   const x = Math.random() * 160 - 80;
   const y = Math.random() * 160 - 80;
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 }
 
+noBtn.addEventListener("mouseover", moveButton);
+noBtn.addEventListener("touchstart", moveButton);
+
+// pindah halaman + play musik
 function goToLove() {
   page1.classList.add("hidden");
   page2.classList.remove("hidden");
+  music.play();
 }
 
+// alasan random
 function nextReason() {
   const random = Math.floor(Math.random() * reasons.length);
   loveText.innerText = reasons[random];
+}
+
+// play / pause musik
+function toggleMusic() {
+  if (music.paused) {
+    music.play();
+  } else {
+    music.pause();
+  }
 }
